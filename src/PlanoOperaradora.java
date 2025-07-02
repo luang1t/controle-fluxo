7b887a7418abb9cb916ba87acc91c16c9659073b
luang1t <luanzragit@gmail.com>
2025-07-02 01:38:34 -0300

    Finalizando com aula de switch

---
+++ src/PlanoOperaradora.java
@@ -0,0 +1,18 @@
+public class PlanoOperaradora {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        switch (sigla) {
+            case "T": {
+                System.out.println("5GB Youtube!");
+            }
+            case "M": {
+                System.out.println("Whats e Insta Grátis!");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação!");
+
+            }
+        }
+    }
+}
